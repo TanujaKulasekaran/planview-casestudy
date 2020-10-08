3753a2c45b6e6b409752dc5a3d2e7b6b3b0b6d19
Tanuja Kulasekaran <Tanuja.Kulasekaran@fisglobal.com>
2020-10-09 05:08:29 +0530

    login page new user validation added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { AuthService } from './../user/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IRegister } from '../register/register';
+import { LoginService } from './login.service';
 
 @Component({
   templateUrl: './login.component.html'
@@ -10,8 +12,12 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   data: any;
+  allEmployees: IRegister[];
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private loginService: LoginService,
+              private router: Router) {
 
   }
 
@@ -20,6 +26,8 @@ export class LoginComponent implements OnInit {
       username: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]],
       password: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9]*'), Validators.minLength(6)]]
     });
+
+    this.getAllEmployees();
   }
 
   login(loginForm) {
@@ -28,15 +36,41 @@ export class LoginComponent implements OnInit {
         const userName = loginForm.value.username;
         console.log('USERNAME ' + userName);
 
+        const password = loginForm.value.password;
+
         this.authService.login(userName);
 
-        if (userName === 'admin') {
-          this.router.navigate(['/adminDashboard']);
+        const isValidUser = this.validateUser(userName, password);
+        console.log('isValidUser ' + isValidUser);
 
+        if (isValidUser && userName === 'admin') {
+          this.router.navigate(['/adminDashboard']);
+        } else if (isValidUser) {
+          this.router.navigate(['/userDashboard']);
         } else {
-        this.router.navigate(['/userDashboard']);
+          this.router.navigate(['/register']);
         }
     }
 
+  }
+
+  validateUser(username: string, password: string): boolean {
+    console.log(this.allEmployees);
+
+    for (const entry of this.allEmployees) {
+      if (entry.username === username && entry.password === password) {
+          console.log('Both username and password are registered');
+          return true;
+      }
+  }
+    return false;
+}
+
+  getAllEmployees() {
+    this.loginService.getAllRegisters().subscribe(
+    (data: IRegister[]) => {
+      this.allEmployees = data;
+    }
+    );
   }
 }
